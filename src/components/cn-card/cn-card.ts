9e7381fcca2c626cd1083ba548542c191d95d0f6
Ville Takanen <vitkukissa@gmail.com>
2024-03-09 10:17:38 +0200

    fix: more card refresh parts

--- src/components/cn-card/cn-card.ts
+++ src/components/cn-card/cn-card.ts
@@ -1,27 +1,50 @@
+import './styles.sass'
 import { html, css, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
 @customElement('cn-card')
 export class CnCard extends LitElement {
   @property({ type: Number, reflect: true })
-  elevation = 0
+  elevation = 1
 
   @property({ type: String, reflect: true })
   cover = ''
+
+  @property({ type: String, reflect: true }) noun = ''
+
+  @property({ type: String, reflect: true }) title = ''
+
+  @property({ type: String, reflect: true }) description = ''
+
+  @property({ type: String, reflect: true }) link = ''
   
   get coverImageURL () {
     return this.cover || ''
   }
 
+  get titleSlot () {
+    if (!this.title) return html``
+
+    if (!this.link) return html`<h4>${this.title}</h4>`
+
+    const linkUrl:string = this.link || ''
+
+    return html`<h4><a href=${linkUrl}>${this.title}</a></h4>`
+  }
+
   render () {
     return html`
+      <div class="cardHeader">   
+        ${this.noun ? html`<cyan-icon noun=${this.noun} class="cardNoun" ?large=${!!this.cover} ?dark=${!!this.cover}></cyan-icon>` : ''}
+        ${this.titleSlot}
+      </div>
       <div class="cardHeader">
         ${this.cover ? html`<img src="${this.coverImageURL}" alt="" class="cover" />` : ''}
         <div class="avatar"><slot name="avatar"></slot></div>
         <div class="title"><slot name="title"></slot></div>
       </div>
       <div class="cardContent">
-        <slot></slot>
+        ${ this.description }
       </div>`
   }
 
@@ -32,6 +55,32 @@ export class CnCard extends LitElement {
       padding: var(--cn-grid-size) var(--cn-gap);
       border-radius: var(--cn-border-radius-large);
       position: relative;
+      container-type: inline-size;
+      flex-grow: 1;
+      transition: background 0.27 ease-in-out;
+    }
+     :host h4 {
+      margin: 12px 16px;
+      padding: 0;
+      font-family: var(--cn-font-family-headings);
+      font-weight: var(--cn-font-weight-headline-card);
+      font-size: var(--cn-font-size-headline-card);
+      line-height: var(--cn-line-height-headline-card);
+      letter-spacing: var(--cn-letter-spacing-headline-card);
+      color: var(--cn-color-heading-card, cyan);
+
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;  
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    :host h4 a {
+      color: var(--cn-color-headings-link, yellow);
+      text-decoration: none;
+    }
+    :host h4 a:hover {
+      text-decoration: underline;
     }
     :host([elevation="0"]) {
       padding: calc(var(--cn-grid-size) - 1px) calc(var(--cn-gap) - 1px);
