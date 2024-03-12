6481e05c6b56dcc2fa724a7668bc0f9c836b7b30
Ville Takanen <vitkukissa@gmail.com>
2024-03-12 12:56:58 +0200

    shims fixed

--- src/solid-shims.d.ts
+++ src/solid-shims.d.ts
@@ -1,12 +1,12 @@
-import { Component } from 'solid-js'; 
-import 'solid-js/types/jsx'; 
-import { CnCard, CnIcon, CnNavigationIcon } from './main';
+import { CnCard } from './components/CnCard'
+import { CnIcon } from './components/CnIcon'
+import { CnNavigationIcon } from './components/CnNavigationIcon'
 
 declare module 'solid-js' {
   namespace JSX {
     interface IntrinsicElements {
-      'cn-icon': CnIcon,
-      'cn-card': CnCard,
+      'cn-icon': CnIcon
+      'cn-card': CnCard
       'cn-navigation-icon': CnNavigationIcon
     }
   }
