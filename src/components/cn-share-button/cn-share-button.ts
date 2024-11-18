5b6208d6d896cf187c5814fd961bc1b5eb92b407
Ville Takanen <vitkukissa@gmail.com>
2024-11-18 13:25:58 +0200

    fix: button sizing polish

--- src/components/cn-share-button/cn-share-button.ts
+++ src/components/cn-share-button/cn-share-button.ts
@@ -27,29 +27,12 @@ export class CnShareButton extends LitElement {
   @property({ type: String, reflect: true })
   noun = ''
 
-  private defaultIcon = html`<svg 
-  version="1.1"
-  xmlns="http://www.w3.org/2000/svg" 
-  xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-  viewBox="0 0 128 128"
-  style="enable-background:new 0 0 128 128;"
-  xml:space="preserve">
-  <style type="text/css">
-  .st0{opacity:0.3;enable-background:new;}
-  </style>
-  <g fill="currentColor">
-    <circle class="st0" cx="102.6" cy="19.3" r="6.4"/>
-    <circle class="st0" cx="25.4" cy="64.3" r="6.4"/>
-    <circle class="st0" cx="102.6" cy="109.4" r="6.4"/>
-    <path d="M102.6,90.5c-4.9,0-9.3,1.9-12.6,4.9L44.1,68.8c0.3-1.5,0.6-3,0.6-4.5s-0.3-3-0.6-4.5l45.3-26.4c3.5,3.2,8,5.2,13.1,5.2
-	  c10.7,0,19.3-8.6,19.3-19.3S113.2,0,102.6,0S83.3,8.6,83.3,19.3c0,1.5,0.3,3,0.6,4.5L38.6,50.2c-3.5-3.2-8-5.2-13.1-5.2
-      C14.8,45,6.2,53.6,6.2,64.3s8.6,19.3,19.3,19.3c5.1,0,9.6-2,13.1-5.2l45.8,26.7c-0.3,1.3-0.5,2.8-0.5,4.2
-      c0,10.3,8.4,18.8,18.8,18.8s18.8-8.4,18.8-18.8S112.9,90.5,102.6,90.5 M102.6,12.9c3.5,0,6.4,2.9,6.4,6.4s-2.9,6.4-6.4,6.4
-      s-6.4-2.9-6.4-6.4S99,12.9,102.6,12.9 M25.4,70.7c-3.5,0-6.4-2.9-6.4-6.4c0-3.5,2.9-6.4,6.4-6.4s6.4,2.9,6.4,6.4
-      C31.9,67.8,29,70.7,25.4,70.7 M102.6,115.8c-3.5,0-6.4-2.9-6.4-6.4c0-3.5,2.9-6.4,6.4-6.4s6.4,2.9,6.4,6.4
-      C109,112.9,106.1,115.8,102.6,115.8"/>
-  </g>
-  </svg>`
+  private defaultIcon = html`<?xml version="1.0" encoding="UTF-8"?>
+  <svg width="1200pt" height="1200pt" version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg">
+   <path fill="currentColor" 
+   d="m1150.2 572.26-412.5-375c-10.969-10.031-26.859-12.562-40.406-6.5625-13.594 6-22.312 19.453-22.312 34.312v161.34c-105.61 0.09375-354.32 23.812-516.84 227.81-81.469 102.28-109.08 282.28-119.48 350.06l-0.79688 5.0625c-2.5312 16.547 6.1406 32.719 21.328 39.75 5.0625 2.3438 10.406 3.4688 15.703 3.4688 10.641 0 21.094-4.5469 28.406-12.984 60.281-69.797 164.9-163.55 320.63-213.84 81.094-26.156 165.32-37.312 250.97-33.141v184.97c0 14.484 8.3438 27.656 21.469 33.891 13.031 6.1406 28.594 4.3125 39.844-4.875l412.5-337.5c8.4375-6.8906 13.453-17.156 13.734-28.031 0.28125-10.922-4.2188-21.375-12.281-28.734z"/>
+  </svg>
+  `
 
   /**
    * Handles the click event on the button.
@@ -124,6 +107,7 @@ export class CnShareButton extends LitElement {
       overflow: hidden;
       flex-grow: 0;
       flex-shrink: 0;
+      padding-right: calc(var(--cn-icon-size-small) + var(--cn-gap));
     }
     :host button:hover{
       color: var(--color-on-cn-share-button-hover);
@@ -154,12 +138,13 @@ export class CnShareButton extends LitElement {
       color: var(--color-on-cn-share-button-active);
     }
 
-    :host svg,
-    :host img {
+    :host svg{
       height: var(--cn-icon-size-small);
       width: var(--cn-icon-size-small);
-      vertical-align: middle;
-      margin-bottom: 0.15em;
+      position: absolute;
+      top: 50%;
+      right: calc(var(--cn-grid) * 1.5);
+      transform: translateY(-53%);
     }
     @media (max-width: 620px) {
       :host .label {
@@ -171,11 +156,6 @@ export class CnShareButton extends LitElement {
         padding: 0;
         padding: var(--cn-grid);
         width: var(--cn-line-height-button, calc(38 / 16 * 1rem));
-      }
-      :host svg {
-        position: absolute;
-        top: var(--cn-grid);
-        left: calc(var(--cn-grid) * 0.75);
       }
     }
     :host img {
@@ -196,7 +176,7 @@ export class CnShareButton extends LitElement {
       }
       :host svg {
         position: absolute;
-        top: 50%;
+        top: 48%;
         left: 50%;
         transform: translate(-50%, -50%) scale(1.25);
       }
